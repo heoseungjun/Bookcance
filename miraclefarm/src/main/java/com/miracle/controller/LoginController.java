a12880c3ac6f9c27581061d2156d1ae99f95eb70
heopaka <gjtmdwns0815@naver.com>
2023-08-08 14:16:12 +0900

    20230808 MiracleFarm login function modify

--- miraclefarm/src/main/java/com/miracle/controller/LoginController.java
+++ miraclefarm/src/main/java/com/miracle/controller/LoginController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,7 +23,7 @@ public class LoginController {
 	}
 	
 	@RequestMapping("/loginimpl")
-	public ModelAndView loginimpl(HttpServletRequest req, ModelAndView mav, HttpSession session) {
+	public ModelAndView loginimpl(HttpServletRequest req, ModelAndView mav, Model model, HttpSession session) {
 		String userid = req.getParameter("userid");
 		String userpwd = req.getParameter("userpwd");
 		CustDto cust = null;
@@ -31,16 +32,19 @@ public class LoginController {
 			cust = service.choice(userid);
 			
 			if(cust == null) {
-				mav.setViewName("redirect:login");
+				model.addAttribute("check", 1);
+				mav.setViewName("redirect:/login");
 			}
 			else {
 				if(cust.getUserpwd().equals(userpwd)) {
 					session.getAttribute(userid);
 					session.getAttribute(userpwd);
+					session.setMaxInactiveInterval(3600);
 					mav.setViewName("redirect:/");
 				}
 				else {
-					mav.setViewName("redirect:login");
+					model.addAttribute("check", 2);
+					mav.setViewName("redirect:/login");
 				}
 			}
 			
